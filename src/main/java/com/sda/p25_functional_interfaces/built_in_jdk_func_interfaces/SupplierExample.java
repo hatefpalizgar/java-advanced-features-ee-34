21a820b6290d2a50fba2f641b93527f77b63a200
hatefpalizgar <hatef.palizgar@gmail.com>
2024-10-13 08:28:48 +0100

    p25_functional_interfaces_built_in_jdk_func_interfaces_Supplier

--- src/main/java/com/sda/p25_functional_interfaces/built_in_jdk_func_interfaces/SupplierExample.java
+++ src/main/java/com/sda/p25_functional_interfaces/built_in_jdk_func_interfaces/SupplierExample.java
@@ -1,5 +1,16 @@
 package com.sda.p25_functional_interfaces.built_in_jdk_func_interfaces;
 
+import java.util.function.Supplier;
+
 public class SupplierExample {
 
+    public static void main(String[] args) {
+        Supplier<Person> personSupplier = () -> new Person("Rio", 29);
+
+        Person rio = personSupplier.get();
+        Person rio2 = personSupplier.get();
+
+        System.out.println(rio.getName()); // Rio
+
+    }
 }
